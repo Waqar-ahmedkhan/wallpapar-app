00fb13241bc6bea0fe62eed929b6f3ff4a51612d
Waqar ahmed <waqarahmed44870@gmail.com>
2024-10-05 11:41:26 +0500

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link, Slot } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-const _layout = () => {
+export default function TabLayout() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        {/* Main content area */}
-        <Slot />
-      </View>
-
-      {/* Bottom navigation bar */}
-      <View style={styles.bottomBar}>
-        <Link href="/" style={styles.tabItem}>
-          <Text style={styles.tabText}>For You</Text>
-        </Link>
-        <Link href="/account" style={styles.tabItem}>
-          <Text style={styles.tabText}>Account</Text>
-        </Link>
-        <Link href="/explore" style={styles.tabItem}>
-          <Text style={styles.tabText}>Explore</Text>
-        </Link>
-      </View>
-    </SafeAreaView>
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'For you',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+       <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
   );
-};
-
-export default _layout;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5F5F5', // Light background color for the entire layout
-  },
-  content: {
-    flex: 1,
-    padding: 10,
-  },
-  bottomBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    paddingVertical: 10,
-    backgroundColor: '#333', // Dark gray or black background
-    borderTopWidth: 1,
-    borderTopColor: '#444', // Slightly lighter border color
-  },
-  tabItem: {
-    paddingVertical: 10,
-    alignItems: 'center',
-  },
-  tabText: {
-    color: '#FFF', // White text color for contrast against dark background
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-});
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Foryou = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Foryou</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
