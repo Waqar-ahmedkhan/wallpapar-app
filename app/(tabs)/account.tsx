7f0ac3f738f239307552a724e09bb5a0abf34b62
Waqar ahmed <waqarahmed44870@gmail.com>
2024-10-16 23:49:26 +0500

    commit

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,53 +1,59 @@
 import React, { useState } from 'react';
-import { Button, Pressable, StyleSheet, Text, useColorScheme } from 'react-native';
+import { Pressable, StyleSheet, Text, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link } from 'expo-router';
-import { DownloadPicture } from '@/components/BottomSheet';
-import { WallpaperType } from '@/hooks/useWallpapar';
 import { Ionicons } from '@expo/vector-icons';
 
 const Account = () => {
-  <SafeAreaView style={ {flex: 1}}>
+  const theme = useColorScheme() ?? 'light';
 
-
-<authButton label={"Login"} icon={<Ionicons name='logo-google' size={24} color={theme ==='light' ? Colors.light.icons : Colors.dark.icons}/>
-}
-
-
-
-
-  </SafeAreaView>
-  
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AuthButton 
+        label="Login" 
+        icon={
+          <Ionicons 
+            name='logo-google' 
+            size={24} 
+            color={theme === 'light' ? 'black' : 'white'} 
+          />
+        } 
+      />
+      <AuthButton 
+        label="signup" 
+        icon={
+          <Ionicons 
+            name='logo-google' 
+            size={24} 
+            color={theme === 'light' ? 'black' : 'white'} 
+          />
+    </SafeAreaView>
+  );
 };
 
 export default Account;
 
-function authButton({icon, label}: {
-  label: string,
-  icon: any
-
-}){
-  const theme = useColorScheme() ?? "light";
-  return <Pressable style={{
-    backgroundColor: "black",
-    padding: 10,
-    marginHorizontal: 40,
-    alignItems: "center",
-    flexDirection: "row",
-    borderRadius: 10
-    }}>
+function AuthButton({ icon, label }) {
+  const theme = useColorScheme() ?? 'light';
+  return (
+    <Pressable style={styles.button}>
       {icon}
-      <Text style={{
-        color: "white",
-        fontWeight: "bold",
-        marginLeft: 10
-      }}>{label}</Text>
-    
-
-  </Pressable>
-
-  
-
+      <Text style={styles.buttonText}>{label}</Text>
+    </Pressable>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    marginHorizontal: 40,
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+});
